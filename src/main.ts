de36336c4b45689e2a5cfe780ca6f63e910c55d6
Jan Dorczak <you@example.com>
2025-12-23 23:24:16 +0100

    commit virginity

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+interface employ {
+  readonly birthdate: Date;
+
+}
+
+const hektor: employ = {
+  birthdate: new Date("1990-01-01"),
+};
+hektor.birthdate = 69.
+document.body.append(hektor.birthdate.toString());
