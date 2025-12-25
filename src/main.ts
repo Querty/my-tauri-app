ebe78f748be497f76874a8aa2a210cd821b52ad7
Jan Dorczak <you@example.com>
2025-12-25 01:41:11 +0100

    authentication failed

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
 import { SpotifyApi,  Scopes} from "@spotify/web-api-ts-sdk";
 
+
 const sdk = SpotifyApi.withUserAuthorization(
   '84a2fed80ef444578096092a097a3aae',
   'http://127.0.0.1:1420/',
   ["user-top-read"]
 );
 
-async function displayTopArtist() {
-  const user = await sdk.currentUser.profile();
-  document.body.append('Welcome' + user.display_name.toString());
-  const topArtist = await sdk.currentUser.topItems('artists', 'long_term', 1);
- 
 
-  if (topArtist.items.length > 0) {
-    const artist = topArtist.items[0];
-    document.body.append('Your top artist is: ' + artist.name.toString());
-  } else {
-    document.body.append('No top artists found.');
+
+async function displayTopArtist() {
+  try {
+    const user = await sdk.currentUser.profile();
+    document.body.append('Welcome ' + user.display_name.toString());
+    const topArtist = await sdk.currentUser.topItems('artists', 'long_term', 1);
+
+    if (topArtist.items.length > 0) {
+      const artist = topArtist.items[0];
+      document.body.append('Your top artist is: ' + artist.name.toString());
+    } else {
+      document.body.append('No top artists found.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    document.body.append('Authentication or API error occurred.');
   }
 }
-
 
 displayTopArtist();
